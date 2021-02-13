83a023b8c5ea04b4cf877bd3d64f959213b408ee
Ryan Mason <rmason@t470.at.home>
2021-02-13 19:56:17 +0800

    Hello

---
+++ .bash_aliases
@@ -0,0 +1,1 @@
+alias dots='cd ~/dots'
---
+++ .config/cmus/lib.pl
@@ -0,0 +1,3 @@
+/home/rmason/Downloads/352cdfe3-fbba-46ef-8982-4b4c37d3da22.mp3
+/home/rmason/Downloads/df800c64-9bac-467b-be5c-088a4cd94882.mp3
+/home/rmason/Downloads/e9e941f3-5d28-4978-9398-058673590033.mp3
